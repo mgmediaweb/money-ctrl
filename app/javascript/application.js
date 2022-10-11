28b6b6f2c968d44d521964cac257dcfe0c8f7e4d
Gonzalo <mgmediaweb@gmail.com>
2022-10-10 21:04:50 -0700

    fix few details

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-const mobileMenu = document.getElementById('movil-menu'); 
-
-const menuClose = () => mobileMenu.classList.remove('menu-show');
-const menuOpen = () => mobileMenu.classList.add('menu-show');
-
-document.getElementById('btn-open').addEventListener('click', menuOpen);
-document.getElementById('btn-close').addEventListener('click', menuClose);
