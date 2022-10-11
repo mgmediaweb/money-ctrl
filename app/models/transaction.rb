16f9f4d41c36031fbb4a07a09ba0d98e9f91a8e2
Gonzalo <mgmediaweb@gmail.com>
2022-10-10 17:17:07 -0700

    implemented devise and improve layout

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,7 +1,4 @@
 class Transaction < ApplicationRecord
   belongs_to :user
-  # has_and_belongs_to_many :categories
   has_and_belongs_to_many :categories
-  # has_many :CategoriesTransactions, dependent: :destroy
-  # has_many :categories, through: :CategoriesTransactions
 end
